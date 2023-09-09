e3766454240b420c90e72d1f4e92f0b1cc1e37d1
mudapa <mudanpa@gmail.com>
2023-09-09 14:46:42 +0700

    upload image base64image

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,9 +7,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use App\Models\Wallet;
+use Melihovv\Base64ImageDecoder\Base64ImageDecoder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class AuthController extends Controller
 {
+    // register
     public function register(Request $request)
     {
         // Validate data
@@ -44,5 +48,38 @@ class AuthController extends Controller
                 409
             );
         }
+
+        // Create user
+        try {
+            $profilePicture = null;
+            $ktp = null;
+
+            if ($request->profile_picture) {
+                $profilePicture = $this->uploadBase64Image($request->profile_picture);
+            }
+
+            if ($request->ktp) {
+                $ktp = $this->uploadBase64Image($request->ktp);
+            }
+        } catch (\Throwable $th) {
+            echo $th;
+        }
+    }
+
+    // Upload base64 image
+    private function uploadBase64Image($base64Image)
+    {
+        // Decode base64 image
+        $decoder = new Base64ImageDecoder($base64Image, $allowedFormats = ['jpeg', 'png', 'gif']);
+        // Get decoded image
+        $decodedContent = $decoder->getDecodedContent();
+        // Get image format
+        $format = $decoder->getFormat();
+        // Generate random image name
+        $image = Str::random(10) . '.' . $format;
+        // Upload image to storage
+        Storage::disk('public')->put($image, $decodedContent);
+
+        return $image;
     }
 }
