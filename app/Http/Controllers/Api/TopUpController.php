ca647777856a92c717aa9078e34a5562f03fa6c9
mudapa <mudanpa@gmail.com>
2023-09-16 02:29:02 +0700

    call to midtrans

--- app/Http/Controllers/Api/TopUpController.php
+++ app/Http/Controllers/Api/TopUpController.php
@@ -69,8 +69,21 @@ class TopUpController extends Controller
                 'status' => 'pending',
             ]);
 
+            // build midtrans parameters
+            $params = $this->buildMidtransParameters([
+                'transaction_code' => $transaction->transaction_code,
+                'amount' => $transaction->amount,
+                'payment_method' => $paymentMethod->code,
+            ]);
+
             // call to midtrans
+            $midtrans = $this->callMidtrans($params);
+
+            // update transaction
             DB::commit();
+
+            // return response
+            return response()->json($midtrans);
         } catch (\Throwable $th) {
             DB::rollBack();
 
@@ -82,5 +95,80 @@ class TopUpController extends Controller
                 500
             );
         }
+    }
+
+    // call to midtrans
+    private function callMidtrans(array $params)
+    {
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = (bool) env('MIDTRANS_IS_PRODUCTION');
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = (bool) env('MIDTRANS_IS_SANITIZED');
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = (bool) env('MIDTRANS_IS_3DS');
+
+        // Call Snap API (https://snap-docs.midtrans.com)
+        $createTransaction = \Midtrans\Snap::createTransaction($params);
+
+        // return url or token
+        return [
+            'redirect_url' => $createTransaction->redirect_url,
+            'token' => $createTransaction->token,
+        ];
+    }
+
+    // build midtrans parameters
+    private function buildMidtransParameters(array $params)
+    {
+        $transactionDetails = [
+            'order_id' => $params['transaction_code'],
+            'gross_amount' => $params['amount'],
+        ];
+
+        $user = auth()->user();
+
+        // split name
+        $spliName = $this->splitName($user->name);
+
+        // customer details
+        $customerDetails = [
+            'first_name' => $spliName['first_name'],
+            'last_name' => $spliName['last_name'],
+            'email' => $user->email,
+            'phone' => $user->phone_number,
+        ];
+
+        // enabled payments
+        $enabledPayment = [
+            $params['payment_method']
+        ];
+
+        // return midtrans parameters
+        return [
+            'transaction_details' => $transactionDetails,
+            'customer_details' => $customerDetails,
+            'enabled_payments' => $enabledPayment,
+        ];
+    }
+
+    // split name
+    private function splitName($fullName)
+    {
+
+        $name = explode(' ', $fullName);
+
+        // case
+        // 'yuki saputra kurnia'
+        // ['yuki', 'saputra', 'kurnia']
+
+        $lastName = count($name) > 1 ? array_pop($name) : $fullName; //kurnia
+        $firstName = implode(' ', $name); //yuki saputra
+
+        return [
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+        ];
     }
 }
