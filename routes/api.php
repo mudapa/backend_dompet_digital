30fa821714c41eec13f8078560c4f415323357ab
mudapa <mudanpa@gmail.com>
2023-10-08 15:53:51 +0700

    data plan controller & histories

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TopUpController;
 use App\Http\Controllers\Api\WebhookController;
 use App\Http\Controllers\Api\TransferController;
+use App\Http\Controllers\Api\DataPlanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,4 +32,5 @@ Route::post('webhooks', [WebhookController::class, 'update']);
 Route::group(['middleware' => 'jwt.verify'], function ($router) {
     Route::post('top_ups', [TopUpController::class, 'store']);
     Route::post('transfers', [TransferController::class, 'store']);
+    Route::post('data_plans', [DataPlanController::class, 'store']);
 });
