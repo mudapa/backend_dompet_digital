ed935b3dfb219cb842e79d9a209633b086ed3fe0
mudapa <mudanpa@gmail.com>
2023-11-24 06:58:55 +0700

    Refactor authentication and sidebar layout
    The commit message suggests that the developer has made changes related
    to authentication and the sidebar layout in the code.

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::guard('web')->attempt($credentials)) {
+            return redirect()->route('admin.dashboard');
+        }
+
+        return redirect()->back()->with('error', 'Invalid credentials')->withInput();
+    }
+
+    public function logout()
+    {
+        Auth::guard('web')->logout();
+        return redirect()->route('admin.auth.index');
+    }
+}
